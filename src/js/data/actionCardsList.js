9ec84336f8972c5df6eebb3844e1a025f26b4e2a
MayorOfBluebell <bramsimonis@live.nl>
2018-01-08 21:51:36 +0100

    Filled in all the Character, Action and Original Challenge cards
    There are still cards in the actionCardsList.js that still have the place holder name, so wether we'll add more cards or not I want to discuss

--- src/js/data/actionCardsList.js
+++ src/js/data/actionCardsList.js
@@ -1,10 +1,543 @@
 export default [
 	{
-		"action": "ASDASDASDASD",
+		"action": "Be supportive",
+		"traits": [
+			{
+				"name": "intelligence",
+				"charisma": 2
+			},
+			{
+				"name": "intelligence",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Tutor your friend",
 		"traits": [
 			{
 				"name": "intelligence",
 				"value": 2
+			},
+			{
+				"name": "perseverance",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Talk your friend through a tough time",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 2
+			},
+			{
+				"name": "charisma",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Be sensitive",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 5
+			},
+			{
+				"name": "pop culture",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Be helpful",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 4
+			},
+			{
+				"name": "pop culture",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Give a helpful speech",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 3
+			},
+			{
+				"name": "empathy",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Help your friend win an argument",
+		"traits": [
+			{
+				"name": "intelligence",
+				"value": 3
+			},
+			{
+				"name": "charisma",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Give a helpful speech",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 5
+			},
+			{
+				"name": "empathy",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Give good instructions",
+		"traits": [
+			{
+				"name": "intelligence",
+				"value": 5
+			},
+			{
+				"name": "charisma",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Help your friend remember something",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 2
+			},
+			{
+				"name": "intelligence",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Be supportive",
+		"traits": [
+			{
+				"name": "intelligence",
+				"value": 2
+			},
+			{
+				"name": "charisma",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Help your friend remember something",
+		"traits": [
+			{
+				"name": "intelligence",
+				"value": 2
+			},
+			{
+				"name": "pop culture",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Lighten the mood",
+		"traits": [
+			{
+				"name": "pop culture",
+				"value": 3
+			},
+			{
+				"name": "perseverance",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Give good instructions",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 3
+			},
+			{
+				"name": "pop culture",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Be a nice friend",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 2
+			},
+			{
+				"name": "charisma",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Be supportive",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 3
+			},
+			{
+				"name": "perseverance",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Help your friend win an argument",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 4
+			},
+			{
+				"name": "perseverance",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Help your friend remember something",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 3
+			},
+			{
+				"name": "intelligence",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Encourage your friend not to give up",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 2
+			},
+			{
+				"name": "empathy",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Give good advice",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 5
+			},
+			{
+				"name": "charisma",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Talk your friend through a tough time",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 4
+			},
+			{
+				"name": "perseverance",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Give good instructions",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 2
+			},
+			{
+				"name": "intelligence",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Encourage your friend not to give up",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 5
+			},
+			{
+				"name": "empathy",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Be sensitive",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 2
+			},
+			{
+				"name": "pop culture",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Tutor your friend",
+		"traits": [
+			{
+				"name": "intelligence",
+				"value": 4
+			},
+			{
+				"name": "empathy",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Recommend a book",
+		"traits": [
+			{
+				"name": "intelligence",
+				"value": 3
+			},
+			{
+				"name": "empathy",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Give a helpful speech",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 3
+			},
+			{
+				"name": "empathy",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Give good instructions",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 4
+			},
+			{
+				"name": "intelligence",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Lighten the mood",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 2
+			},
+			{
+				"name": "perseverance",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Give good advice",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 3
+			},
+			{
+				"name": "pop culture",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Recommend a book",
+		"traits": [
+			{
+				"name": "intelligence",
+				"value": 2
+			},
+			{
+				"name": "perseverance",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Recommend a book",
+		"traits": [
+			{
+				"name": "intelligence",
+				"value": 2
+			},
+			{
+				"name": "pop culture",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Encourage your friend not to give up",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 2
+			},
+			{
+				"name": "perseverance",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Be a nice friend",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 3
+			},
+			{
+				"name": "pop culture",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Lighten the mood",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 5
+			},
+			{
+				"name": "pop culture",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Be helpful",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 5
+			},
+			{
+				"name": "pop culture",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Help your friend win an argument",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 2
+			},
+			{
+				"name": "intelligence",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Give a helpful speech",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 2
+			},
+			{
+				"name": "empathy",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Help your friend remember something",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 2
+			},
+			{
+				"name": "pop culture",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Use your knowledge",
+		"traits": [
+			{
+				"name": "intelligence",
+				"value": 4
+			},
+			{
+				"name": "pop culture",
+				"value": 1
+			}
+		]
+	},
+	{
+		"action": "Be a nice friend",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 3
+			},
+			{
+				"name": "pop culture",
+				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Be helpful",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 3
 			},
 			{
 				"name": "pop culture",
@@ -13,11 +546,50 @@ export default [
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Recommend a book",
 		"traits": [
 			{
 				"name": "intelligence",
+				"value": 3
+			},
+			{
+				"name": "empathy",
 				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Give good advice",
+		"traits": [
+			{
+				"name": "empathy",
+				"value": 3
+			},
+			{
+				"name": "perseverance",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Be helpful",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 4
+			},
+			{
+				"name": "pop culture",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Use your knowledge",
+		"traits": [
+			{
+				"name": "intelligence",
+				"value": 1
 			},
 			{
 				"name": "pop culture",
@@ -26,11 +598,63 @@ export default [
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Be sensitive",
 		"traits": [
 			{
-				"name": "intelligence",
+				"name": "empathy",
+				"value": 1
+			},
+			{
+				"name": "pop culture",
+				"value": 5
+			}
+		]
+	},
+	{
+		"action": "Lighten the mood",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 4
+			},
+			{
+				"name": "pop culture",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Talk your friend through a tough time",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 3
+			},
+			{
+				"name": "charisma",
 				"value": 2
+			}
+		]
+	},
+	{
+		"action": "Be sensitive",
+		"traits": [
+			{
+				"name": "charisma",
+				"value": 2
+			},
+			{
+				"name": "pop culture",
+				"value": 3
+			}
+		]
+	},
+	{
+		"action": "Give good advice",
+		"traits": [
+			{
+				"name": "perseverance",
+				"value": 1
 			},
 			{
 				"name": "pop culture",
@@ -39,85 +663,85 @@ export default [
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Be supportive",
 		"traits": [
 			{
 				"name": "intelligence",
 				"value": 2
 			},
 			{
-				"name": "pop culture",
-				"value": 4
+				"name": "charisma",
+				"value": 3
 			}
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Be a nice friend",
 		"traits": [
 			{
-				"name": "intelligence",
-				"value": 2
+				"name": "charisma",
+				"value": 1
 			},
 			{
 				"name": "pop culture",
-				"value": 4
+				"value": 5
 			}
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Talk your friend through a tough time",
 		"traits": [
 			{
-				"name": "intelligence",
-				"value": 2
+				"name": "charisma",
+				"value": 3
 			},
 			{
-				"name": "pop culture",
-				"value": 4
+				"name": "perseverance",
+				"value": 3
 			}
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Use your knowledge",
 		"traits": [
 			{
 				"name": "intelligence",
-				"value": 2
+				"value": 3
 			},
 			{
 				"name": "pop culture",
-				"value": 4
+				"value": 2
 			}
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Help your friend win an argument",
 		"traits": [
 			{
 				"name": "intelligence",
-				"value": 2
+				"value": 4
 			},
 			{
-				"name": "pop culture",
-				"value": 4
+				"name": "perseverance",
+				"value": 1
 			}
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Tutor your friend",
 		"traits": [
 			{
 				"name": "intelligence",
-				"value": 2
+				"value": 3
 			},
 			{
-				"name": "pop culture",
-				"value": 4
+				"name": "perseverance",
+				"value": 3
 			}
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Use your knowledge",
 		"traits": [
 			{
 				"name": "intelligence",
@@ -125,656 +749,32 @@ export default [
 			},
 			{
 				"name": "pop culture",
-				"value": 4
+				"value": 3
 			}
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Encourage your friend not to give up",
 		"traits": [
 			{
-				"name": "intelligence",
-				"value": 2
+				"name": "perseverance",
+				"value": 4
 			},
 			{
-				"name": "pop culture",
-				"value": 4
+				"name": "charisma",
+				"value": 1
 			}
 		]
 	},
 	{
-		"action": "ASDASDASDASD",
+		"action": "Tutor your friend",
 		"traits": [
 			{
 				"name": "intelligence",
-				"value": 2
+				"value": 1
 			},
 			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
-				"value": 4
-			}
-		]
-	},
-	{
-		"action": "ASDASDASDASD",
-		"traits": [
-			{
-				"name": "intelligence",
-				"value": 2
-			},
-			{
-				"name": "pop culture",
+				"name": "empathy",
 				"value": 4
 			}
 		]
